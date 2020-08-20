5dc202b324b2553b9332274006b0f2d60ad49e16
Mark <plambiroo@gmail.com>
2020-08-20 21:23:48 +0300

    set serializer for CustomersPagedList

--- src/main/java/guru/model/CustomersPagedList.java
+++ src/main/java/guru/model/CustomersPagedList.java
@@ -1,11 +1,32 @@
 package guru.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.io.Serializable;
 import java.util.List;
 
-public class CustomersPagedList extends PageImpl<CustomerDto> {
+public class CustomersPagedList extends PageImpl<CustomerDto> implements Serializable {
+
+    static final long serialVersionUID = -7115447482071388357L;
+
+    @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
+    public CustomersPagedList(@JsonProperty("content") List<CustomerDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number, size), totalElements);
+    }
 
     public CustomersPagedList(List<CustomerDto> content, Pageable pageable, long total) {
         super(content, pageable, total);
